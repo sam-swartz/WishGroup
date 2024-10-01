549c8dd1bf3db07c1162e6535d30890168fbb0d2
sam-swartz <samsuvarts12@gmail.com.com>
2024-10-01 17:10:02 +0530

    Initial commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,3 +45,52 @@ $(document).ready(function() {
         $(this).css('transform', 'perspective(1000px) rotateX(0) rotateY(0) translateZ(0)');
     });
 });
+
+
+
+// oneBond custom card
+
+$(document).ready(function() {
+    function createOneBondParticles($cardElement) {
+        for (let i = 0; i < 20; i++) {
+            let $oneBondParticle = $('<div>').addClass('oneBond_card_particle');
+            let oneBondParticleSize = Math.random() * 5 + 2;
+            $oneBondParticle.css({
+                width: oneBondParticleSize + 'px',
+                height: oneBondParticleSize + 'px',
+                left: Math.random() * 100 + '%',
+                top: Math.random() * 100 + '%',
+                animationDuration: (Math.random() * 2 + 1) + 's'
+            });
+            $cardElement.append($oneBondParticle);
+        }
+    }
+
+    $('.oneBond_info_card').each(function() {
+        var $oneBondCard = $(this);
+        createOneBondParticles($oneBondCard);
+
+        $oneBondCard.on('mousemove', function(oneBondEvent) {
+            var oneBondRect = $oneBondCard[0].getBoundingClientRect();
+            var oneBondMouseX = oneBondEvent.clientX - oneBondRect.left;
+            var oneBondMouseY = oneBondEvent.clientY - oneBondRect.top;
+            $oneBondCard.css({
+                '--oneBond-mouse-x': oneBondMouseX + 'px',
+                '--oneBond-mouse-y': oneBondMouseY + 'px'
+            });
+        });
+
+        // Add 3D tilt effect
+        $oneBondCard.on('mousemove', function(e) {
+            var cardRect = $oneBondCard[0].getBoundingClientRect();
+            var cardCenterX = cardRect.left + cardRect.width / 2;
+            var cardCenterY = cardRect.top + cardRect.height / 2;
+            var angleY = -(e.clientX - cardCenterX) / 25;
+            var angleX = (e.clientY - cardCenterY) / 25;
+
+            $oneBondCard.css('transform', `perspective(2000px) rotateX(${angleX}deg) rotateY(${angleY}deg) translateZ(10px)`);
+        }).on('mouseleave', function() {
+            $oneBondCard.css('transform', 'none');
+        });
+    });
+});
