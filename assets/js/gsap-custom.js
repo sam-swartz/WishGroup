5f60b08b15a92b183636a086cccfcb8c9332987c
sam-swartz <samsuvarts12@gmail.com.com>
2024-10-05 14:22:40 +0530

    Initial commit

--- assets/js/gsap-custom.js
+++ assets/js/gsap-custom.js
@@ -1,42 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Pin the title section
+ScrollTrigger.create({
+  trigger: ".our-business_banner",
+  start: "top top",
+  endTrigger: ".our-business_section",
+  end: "bottom top", // Ends when the first our-business_section hits the top
+  pin: true,
+  pinSpacing: false
+});
 
-// <-- banner Animation Start here -->
+const sections = gsap.utils.toArray('.our-business_section');
 
+sections.forEach((section) => {
 
-
-// const missionCard = gsap.timeline({ defaults: { duration: 1 } });
-
-// missionCard.fromTo("#oneBond_mission_card", {
-//         opacity: 0,
-//         x: -500,
-//     }, {
-//         x: 0,
-//         ease: "power1.in",
-//         opacity: 1,
-//     })
-//     .fromTo("#oneBond_values_card", {
-//         opacity: 0,
-//         x: -500,
-//     }, {
-//         x: 0,
-//         ease: "power1.in",
-//         opacity: 1,
-//     })
-//     .fromTo("#oneBond_integrity_card", {
-//         opacity: 0,
-//         x: -500,
-//     }, {
-//         x: 0,
-//         ease: "power1.in",
-//         opacity: 1,
-//     });
-
-// ScrollTrigger.create({
-//     animation: missionCard,
-//     trigger: "#mission",
-//     start: "top bottom",
-//     end: "+=1000",
-//     scrub: true,
-//     markers: true
-// });
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top top",  // Start pinning when the section hits the top of the viewport
+    pin: true,
+    pinSpacing: false,  // Remove default spacing, so sections overlap perfectly
+    scrub: true,        // Smooth scrolling effect
+    snap: {
+      snapTo: 1 / (sections.length - 1),  // Snap to each section
+      duration: { min: 0.2, max: 0.5 },   // Smooth snapping
+      ease: "power1.inOut"
+    },
+    markers: false,     // Remove markers for clean UI
+  });
+});
