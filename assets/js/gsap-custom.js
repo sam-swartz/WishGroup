b65356b5524b33d685eea777a339c2290e279592
sam-swartz <samsuvarts12@gmail.com.com>
2024-10-06 18:25:17 +0530

    Initial commit

--- assets/js/gsap-custom.js
+++ assets/js/gsap-custom.js
@@ -2,24 +2,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initScrollAnimations() {
   const sections = gsap.utils.toArray('.our-business_section');
-  
+
   // Pin the title section
   ScrollTrigger.create({
     trigger: ".our-business_banner",
     start: "top top",
     endTrigger: ".our-business_section",
-    end: "bottom top", 
+    end: "bottom top",
     pin: true,
     pinSpacing: false
   });
 
   sections.forEach((section, index) => {
+    const isLastSection = index === sections.length - 1;
+
     ScrollTrigger.create({
       trigger: section,
       start: "top top",
+      end: isLastSection ? "bottom bottom" : "bottom top",
       pin: true,
-      pinSpacing: false,  
-      scrub: true,       
+      pinSpacing: false,
+      scrub: true,
       snap: {
         snapTo: 1 / (sections.length - 1),
         duration: { min: 0.2, max: 0.5 },
@@ -39,12 +42,22 @@ function initScrollAnimations() {
       }
     });
   });
+
+  // Add a separate trigger for the footer
+  ScrollTrigger.create({
+    trigger: "footer",
+    start: "top bottom",
+    end: "bottom bottom",
+    scrub: true,
+    onEnter: () => gsap.to("footer", { opacity: 1, duration: 0.5 }),
+    onLeaveBack: () => gsap.to("footer", { opacity: 0, duration: 0.5 })
+  });
 }
 
 function handleResize() {
   // Kill all ScrollTrigger instances
   ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-  
+
   // Reinitialize animations
   initScrollAnimations();
 }
